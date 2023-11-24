b026ae0de7a655ee046b00349208a663d7cb2716
robin nganga smith <saintofcookies.2021@gmail.com>
2023-11-24 17:32:01 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    
+    return [num ** 2 for num in int_list]
+    
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(100):
+        print(i)
+        while i == 3:
+            print("Fizz")
+            while i == 5:
+                print("Buzz")
+                while i == 3 and 5:
+                    print("FizzBuzz")
